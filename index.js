9dff1579ab32ee268c512a5297f22ebe9ee90924
okshi <johnstarhafiz@gmail.com>
2022-01-26 09:37:01 +0600

    Start Travel Agency Server and set up mongoDB conncetion

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+var ObjectId = require("mongodb").ObjectId;
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.luy9u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// console.log(uri);
+
+async function run() {
+  
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello Wivo!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
